9d9abf081510f90948ebabb2caa64bdebd9905b6
Grecia Delgado <greciadm2596@gmail.com>
2025-08-19 21:18:49 -0500

    update and create CRUD

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import { users, products, transactions } from "./data/index.js";
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log(`${req.method} ${req.url}`);
   res.send("HOLA MUNDO ESTOY USANDO EXPRESSSSS");
 });
 
+// RUTA para listar todos los usuarios
 app.get("/users", (req, res) => {
   res.send(users);
 });
 
+// RUTA para lista 1 usurio en especifico
 app.get("/users/:userId", (req, res) => {
   const userId = req.params.userId;
 
@@ -21,6 +25,27 @@ app.get("/users/:userId", (req, res) => {
 
   res.json({ data: findUser, code: 200 });
 });
+
+//RUTA para CREAR un usurio
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  // faltaria agregar una condicion , para decir que si esta repetido el user ID no lo agregue
+
+  users.data.push(newUser);
+
+  res.json({ message: "new user was added into users.json", data: users });
+});
+
+//RUTA para hacer UPDATE a un usurio
+app.patch("users/:userId", (req, res)=> {
+  // tarea : implemnetra el pacth
+})
+
+//RUTA para ELIMINAR a un usuario
+app.delete("users/:userId", (req, res)=> {
+  // tarea : implementar el DELETE de un usauario
+})
+
 
 app.get("/products", (req, res) => {
   res.send(products);
